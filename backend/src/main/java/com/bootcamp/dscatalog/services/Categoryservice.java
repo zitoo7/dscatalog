8d8bf00827ad938fd96e94e8fe0ad9398a74ccce
João Soares <zitto_7@hotmail.com>
2022-01-22 23:30:23 +0000

    Database seeding

--- backend/src/main/java/com/bootcamp/dscatalog/services/Categoryservice.java
+++ backend/src/main/java/com/bootcamp/dscatalog/services/Categoryservice.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bootcamp.dscatalog.entities.Category;
 import com.bootcamp.dscatalog.repositories.CategoryRepository;
@@ -14,8 +15,10 @@ public class Categoryservice {
 	@Autowired	
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
 	
 }
+ 
