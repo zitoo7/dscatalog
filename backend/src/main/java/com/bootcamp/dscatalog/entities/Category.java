9946f2bec753ad389b02e1e4966da4224b93d0ea
João Soares <zitto_7@hotmail.com>
2022-01-22 17:52:26 +0000

    CategoryResource

---
+++ backend/src/main/java/com/bootcamp/dscatalog/entities/Category.java
@@ -0,0 +1,64 @@
+package com.bootcamp.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	//private Set<Product> products = new HashSet<>();
+	
+	public Category() {}
+
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}	
+}
