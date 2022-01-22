477a6128510e9e0b17cc45b4b89302aa752e43d7
João Soares <zitto_7@hotmail.com>
2022-01-22 23:54:02 +0000

    Category DTO

--- backend/src/main/java/com/bootcamp/dscatalog/resources/categoryResource.java
+++ backend/src/main/java/com/bootcamp/dscatalog/resources/categoryResource.java
@@ -1,30 +1,27 @@
 package com.bootcamp.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bootcamp.dscatalog.entities.Category;
-import com.bootcamp.dscatalog.repositories.CategoryRepository;
+import com.bootcamp.dscatalog.dto.CategoryDTO;
+import com.bootcamp.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class categoryResource {
 	
-	@Autowired
-	CategoryRepository categoryRepository;
+	@Autowired	
+	CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
+	public ResponseEntity<List<CategoryDTO>> findAll(){
 		
-		List<Category> list = categoryRepository.findAll();
-		
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
--- backend/src/main/java/com/bootcamp/dscatalog/services/Categoryservice.java
+++ backend/src/main/java/com/bootcamp/dscatalog/services/Categoryservice.java
@@ -1,23 +1,27 @@
 package com.bootcamp.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bootcamp.dscatalog.dto.CategoryDTO;
 import com.bootcamp.dscatalog.entities.Category;
 import com.bootcamp.dscatalog.repositories.CategoryRepository;
 
 @Service
-public class Categoryservice {
+public class CategoryService {
 
 	@Autowired	
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDTO;
 	}
 	
 }
